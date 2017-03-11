c8ff7ad53f1592906bc4b73eac9638b4af3446b6
BeMg <gccbg04538@gmail.com>
2017-03-11 08:10:08 -0800

    add easy  webcam geter

---
+++ GetImageforCam.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    flag, img = cap.read()
+    cv2.imshow('GGGG', img)
+
+    if cv2.waitKey(5) == 27:
+        cap.release()
