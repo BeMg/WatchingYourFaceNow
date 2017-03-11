7babd9915220502b655c565fa20440c0246e672c
BeMg <gccbg04538@gmail.com>
2017-03-11 07:34:17 -0800

    work itself

--- sample/facedetect.py
+++ sample/facedetect.py
@@ -45,9 +45,9 @@ if __name__ == '__main__':
         video_src = 0
     args = dict(args)
     cascade_fn = args.get(
-        '--cascade', "../../data/haarcascades/haarcascade_frontalface_alt.xml")
+        '--cascade', "../haarcascades/haarcascade_frontalface_alt.xml")
     nested_fn = args.get('--nested-cascade',
-                         "../../data/haarcascades/haarcascade_eye.xml")
+                         "../haarcascades/haarcascade_eye.xml")
 
     cascade = cv2.CascadeClassifier(cascade_fn)
     nested = cv2.CascadeClassifier(nested_fn)
