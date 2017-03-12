ee84202700372d7ef547b2208dc43c40f1284772
BeMg <gccbg04538@gmail.com>
2017-03-11 22:44:49 -0800

    move function to a file

--- DetectFacefromimg.py
+++ DetectFacefromimg.py
@@ -1,19 +1,6 @@
 import cv2
 import numpy as np
-
-
-def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(10, 10),
-                                     flags=cv2.CASCADE_SCALE_IMAGE)
-    if len(rects) == 0:
-        return []
-    rects[:, 2:] += rects[:, :2]
-    return rects
-
-
-def draw(img, rects, color):
-    for x1, y1, x2, y2 in rects:
-        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+from detectanddraw import detect, draw
 
 
 img = cv2.imread('./data/media/0.jpg', 1)
---
+++ detectanddraw.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(10, 10),
+                                     flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:, 2:] += rects[:, :2]
+    return rects
+
+
+def draw(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
