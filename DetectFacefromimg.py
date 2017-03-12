fa7b248839489090d741d815cf2f77a8b99f3ea6
BeMg <gccbg04538@gmail.com>
2017-03-11 22:41:11 -0800

    success detect

--- DetectFacefromimg.py
+++ DetectFacefromimg.py
@@ -11,7 +11,13 @@ def detect(img, cascade):
     return rects
 
 
-img = cv2.imread('./data/media/0.jpg', 0)
+def draw(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+
+img = cv2.imread('./data/media/0.jpg', 1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cascade_xml = []
 
@@ -19,13 +25,18 @@ cascade_xml.append('./data/haarcascades/haarcascade_frontalface_alt.xml')
 cascade_xml.append('./data/haarcascades/haarcascade_frontalface_alt2.xml')
 cascade_xml.append('./data/haarcascades/haarcascade_frontalface_default.xml')
 cascade_xml.append('./data/haarcascades/haarcascade_frontalface_alt_tree.xml')
-cascade_xml.append('./data/haarcascades/haarcascade_fullbody.xml')
 
 cascades = []
 
 for i, xml in enumerate(cascade_xml):
     cascades.append(cv2.CascadeClassifier(xml))
 
-for i, cascade in enumerate(cascades):
-    rects = detect(img, cascade)
-    print("{}: {}".format(i, rects))
+rects = []
+
+for cascade in cascades:
+    rects.append(detect(gray, cascade))
+    print(detect(gray, cascade))
+draw(img, rects[0], (255, 0, 0))
+
+cv2.imshow('aaa', img)
+cv2.waitKey(0)
